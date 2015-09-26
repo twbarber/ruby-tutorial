d105e08ddbb4ff0283af4d1d12fed015f01b8054
twbarber <tylerwbarber@gmail.com>
2015-09-26 14:14:08 +0000

    experimenting with routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  def root
+    render text: "Hello, root!"
+  end
+  
   def hello
     render text: "¡Hola, mundo!"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#goodbye'
+  root 'application#root'
+  get 'hello' => 'application#hello'
+  get 'goodbye' => 'application#goodbye'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
